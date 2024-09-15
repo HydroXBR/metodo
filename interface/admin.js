515d621237d06de6a39dce40254528ec56459785
Psy <72590492+HydroXBR@users.noreply.github.com>
2024-09-15 16:28:31 -0400

    Update admin.js

--- interface/admin.js
+++ interface/admin.js
@@ -77,6 +77,30 @@ class User {
 	}
 }
 
+async function alunos(){
+	const response = await fetch(`/alunos`)
+	console.log("fetched alunos")
+	const rr = await response
+	if(!rr) return false
+	return rr
+}
+
+async function setpago(id, mes, set){
+	const response = await fetch(`/setpago?id=${id}&mes=${mes}&set=${set}`)
+	console.log(`Set pago: ${id} ${mes} ${set}`)
+	const rr = await response
+	if(!rr) return false
+	return rr
+}
+
+async function getalunobyid(id){
+	const response = await fetch(`/getalunobyid?id=${id}`)
+	console.log("fetched aluno: ", id)
+	const rr = await response
+	if(!rr) return false
+	return rr
+}
+
 document.addEventListener('DOMContentLoaded', function() {
 	if (User.isLoggedIn()) {
 		const userInfo = User.getUserInfo();
@@ -95,7 +119,11 @@ document.addEventListener('DOMContentLoaded', function() {
 
 		if(userInfo.profilePicture !== 'null'&& userInfo.profilePicture) {
 			document.getElementById("profile").src = userInfo.profilePicture;
+			document.getElementById("profile2").src = userInfo.profilePicture;
 		}
+
+		document.getElementById("admin").innerText = userInfo.completename;
+		document.getElementById("level").innerText = userInfo.permissions;
 
 		if(Number(userInfo.permissions) == 0){
 			return document.body.innerHTML = "Não autorizado!"
@@ -112,5 +140,86 @@ document.addEventListener('DOMContentLoaded', function() {
 		console.log("Usuário não está logado.");
 		document.body.innerHTML = "Não autorizado!"
 		return window.location.href="/login"
+	}
+
+	function formatname(fullName) {
+		const words = fullName.trim().split(/\s+/);
+
+		const firstName = words[0]; 
+		const secondName = words[1];
+
+		if(secondName){
+		if (["de", "da", "dos", "das", "do", "henrique", "pedro", "eduarda", "luiza"].includes(secondName.toLowerCase())) {
+			if (["de", "da", "dos", "das", "do"].includes(words[2].toLowerCase())) {
+				return `${firstName} ${secondName} ${words[2]} ${words[3]}`;
+			} else {
+				return `${firstName} ${secondName} ${words[2]}`;
+			}
+		}
+		}
+		return `${firstName} ${secondName}`;
+	}
+	
+	preencherTabela()
+	function preencherTabela(){
+		alunos().then(async response => {
+		const todos = await response.json();
+
+		const today = new Date();
+		const data = todos.filter(aluno => aluno.dia == today.getDate())
+		data.sort((a, b) => a.completename.localeCompare(b.completename));
+			
+		const tableBody = document.getElementById('table-body');
+		tableBody.innerHTML = '';
+
+		if(data.length == 0){
+			document.getElementById("tablediv").style.display = "none"
+			let pp = document.createElement("p")
+			pp.innerText = "Não há pagamentos a receber hoje."
+			return document.getElementById("pgtos").appendChild(pp)
+		}
+
+		data.forEach((aluno, index) => {
+			const row = document.createElement('tr');
+
+			const nCell = document.createElement('td');
+			nCell.innerText = index + 1
+			nCell.classList.add("ncell")
+			row.appendChild(nCell);
+
+			const nomeCell = document.createElement('td');
+			let a = document.createElement('a')
+			a.href = `/aluno?id=`+aluno._id
+			a.innerText = formatname(aluno.completename)
+			nomeCell.appendChild(a)
+			row.appendChild(nomeCell);
+
+			const turmaCell = document.createElement('td');
+			turmaCell.innerText = aluno.turma + "°"
+			turmaCell.classList.add("ncell")
+			row.appendChild(turmaCell);
+
+			function formatWhatsappLink(phoneNumber) {
+				const formattedNumber = phoneNumber.trim().replace(/ /gmi, "").slice(0, 2) + phoneNumber.slice(3);
+				const parenteses = formattedNumber.replace(/\(/gmi, "")
+				const parenteses2 = parenteses.replace(/\)/gmi, "")
+				const parenteses3 = parenteses2.replace(/\-/gmi, "")
+				const whatsappLink = `https://api.whatsapp.com/send/?phone=55${parenteses3.trim().replace(/ /gmi, "")}`;
+				return whatsappLink;
+			}
+
+			const enviarCell = document.createElement('td');
+			let a2 = document.createElement('a')
+			a2.href = formatWhatsappLink(aluno.telresp)
+			a2.innerText = "Mensagem"
+			a2.classList.add("btn-message")
+			enviarCell.appendChild(a2)
+			row.appendChild(enviarCell);
+
+			tableBody.appendChild(row);
+		});
+		}).catch(error => {
+			console.error('Erro ao obter dados dos alunos:', error);
+		});
 	}
 })
