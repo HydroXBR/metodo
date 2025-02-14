7f38051aaf0b86a3f04593f37edfc94fbffd2ea3
Psy <72590492+HydroXBR@users.noreply.github.com>
2025-02-13 23:57:20 -0400

    Update admin.js
    hamburguer

--- interface/admin.js
+++ interface/admin.js
@@ -1,3 +1,18 @@
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	navLinks.classList.toggle('active');
+});
+
+document.querySelectorAll('.nav-links a').forEach(link => {
+	link.addEventListener('click', () => {
+		hamburger.classList.remove('active');
+		navLinks.classList.remove('active');
+	});
+});
+
 // Classe Cookie para manipular cookies
 class Cookie {
 	static set(name, value, days) {
