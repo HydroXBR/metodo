97929e34e83301f0ac30761b7538ace349555179
Psy <72590492+HydroXBR@users.noreply.github.com>
2025-02-14 01:15:31 -0400

    Update login.js

--- interface/login.js
+++ interface/login.js
@@ -1,3 +1,18 @@
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	navLinks.classList.toggle('active');
+});
+
+document.querySelectorAll('.nav-links a').forEach(link => {
+	link.addEventListener('click', () => {
+		hamburger.classList.remove('active');
+		navLinks.classList.remove('active');
+	});
+});
+
 async function addd(email, senha){
 	const response = await fetch(`/logar?email=${email}&senha=${senha}`)
 	console.log("addded")
